2b5e695499cd67a185d6510a49a2700caa52d450
Ali-Said1 <asazizg1@gmail.com>
2021-08-06 15:40:11 +0200

    Source code button Added

--- js/timeoutScript.js
+++ js/timeoutScript.js
@@ -1,3 +1,9 @@
 function returnToMain() {
   window.open("index.html", "_self", "width:100vw", "height:100vh");
 }
+function sourceCode() {
+  window.open(
+    "https://github.com/Ali-Said1/Changeable-Timeout-using-js",
+    "_self"
+  );
+}
